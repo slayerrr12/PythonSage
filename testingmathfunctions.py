7299b950152e1d0427c6d3d2c99c6c6f64672bc7
slayerrr12 <eyeamarinsharma@gmail.com>
2023-09-01 12:49:39 +0530

    testing bin fucntion

--- testingmathfunctions.py
+++ testingmathfunctions.py
@@ -1,5 +1,11 @@
-import math 
+import math
 
 print(abs(math.ceil(-4.09113)))
-print(math.factorial(11))
 
+print(math.factorial(11))
+print(math.fabs(-8772731.7173))
+
+a = 213 + 991j
+b = 213 - 1312j
+
+print(bin(5))
