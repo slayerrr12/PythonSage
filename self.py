3f0a8c3f1fd4e263e15f12b7d297f8b2052ef5c5
slayerrr12 <eyeamarinsharma@gmail.com>
2023-09-07 02:00:58 +0530

    understanding self

---
+++ self.py
@@ -0,0 +1,15 @@
+class Point : 
+    def __init__(self,x,y) :
+        self.x = x 
+        self.y = y 
+        
+    def tellValue(self) : 
+        print(f"X = {self.x} Y = {self.y}")
+
+
+
+p1 = Point(10,20)
+p1.tellValue()
+        
+        
+        
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-a = [1,3,2,3,1,3,1,1]
-
-# #this will simply print all items inside the list 
-# for k in a :
-#     print(k)
-#     print(type(k))
-    
+self.y
