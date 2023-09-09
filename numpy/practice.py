65416edeac9c77977286bd1a959fcc487e82c7bf
slayerrr12 <eyeamarinsharma@gmail.com>
2023-09-09 15:43:58 +0530

    why do we use numpy

--- numpy/practice.py
+++ numpy/practice.py
@@ -1,2 +1,11 @@
 #done with installing jupter notebook
 #done with installing numpy
+
+
+# why use numpy 
+
+# efficient storage 
+# better ways of handling data
+# it is fast
+# easy to learn 
+# uses less memory to store data
