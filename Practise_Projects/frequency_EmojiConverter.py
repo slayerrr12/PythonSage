e6547a1a21887ef86008b6a77ddafe12639d9bd3
Harsh Shrivastava <harshshrivastava554@gmail.com>
2023-09-10 18:29:35 +0530

    Data structures python

---
+++ Practise_Projects/frequency_EmojiConverter.py
@@ -0,0 +1,12 @@
+message = "good morning :)"
+words = message.split(' ')
+emojis = {
+    ":)" : "😊",
+    ":(" : "😒",
+    ":|" : "😑"
+}
+output = ""
+for word in words:
+    output+= emojis.get(word, word) +" "
+    
+print(output)
