53fefc3e88870b0034b25daa6e810693c1047c17
slayerrr12 <eyeamarinsharma@gmail.com>
2023-09-10 22:43:00 +0530

    slcing of 2d arrays

---
+++ slicingOfArrays.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+
+a1 = np.array([[1, 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9]])
+
+print(a1[0:3,0:3])
+
+a1 = np.array(list(range(10)))
+print(a1[0:3])
+
