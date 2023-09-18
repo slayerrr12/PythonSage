237aa7fc0f88b0147537802116fb3ab8002ec41c
slayerrr12 <eyeamarinsharma@gmail.com>
2023-09-18 18:40:32 +0530

    changing the casing of the code

--- Cool Scripts/frequency_passwordGenrator.py
+++ Cool Scripts/frequency_passwordGenrator.py
@@ -1,6 +1,6 @@
 import random
 import string
-def Password_generator(length, list1, list2, list3, list4):
+def password_generator(length, list1, list2, list3, list4):
     password = ""
     for i in range(length):
         rand = random.randint(1, 4)  # Generate a random number between 1 and 4 (inclusive).
@@ -20,4 +20,4 @@ list_digit = string.digits
 list_alpha_lower = string.ascii_lowercase
 list_alpha_upper = string.ascii_uppercase
 
-print(Password_generator(8, list_alpha_upper, list_alpha_lower, list_digit, list_special_symbol))
+print(password_generator(8, list_alpha_upper, list_alpha_lower, list_digit, list_special_symbol))
